97125c5159ab1327a2c9e89eb82044e41e996a23
Kristijan Aleksic <kristijan.aleksic@gmail.com>
2022-06-15 12:56:31 +0200

    finished

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ morgan.token('body',  (req, res) => {
   return `${JSON.stringify(req.body)}`
 })
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(morgan(':body'))
 
 //URL to DB
 const url = "x"
